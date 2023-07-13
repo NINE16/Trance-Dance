d20eb285ce9140852046239dba3fae125307e423
NINE16 <74472310+NINE16@users.noreply.github.com>
2023-07-13 16:27:25 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey losers! <br>
+          </br>
+          Trans & Dance
+        </p>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;

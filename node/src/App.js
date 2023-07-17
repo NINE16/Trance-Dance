ecad9cdf403b74f0fd0d6338e10297debe173674
Nikki <nikki@admins-MacBook-Pro.local>
2023-07-17 17:35:11 +0200

    cowww

---
+++ node/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey losers! <br>
+          </br>
+          Trans & Dance let's gooo
+        </p>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
